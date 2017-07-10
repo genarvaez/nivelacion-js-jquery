0ef2446ea445cd2d13aa1366aeb24f0ed2050371
genarvaez <ge_naravez@hotmail.com>
2017-07-10 14:00:23 -0400

    subiendo avance hasta 2pm

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
 	console.log("El documento está listo")
 	$(".icon-menu").hide();
 	$(".icon-arrow-left-alt").show();
+
+	$(".js-show-make").click(function(){
+		$(".page .recipe").addClass('make');
+	})
+	$(".js-show-recipe").click(function(){
+		$(".page .recipe").removeClass('make')
+	})
 })
