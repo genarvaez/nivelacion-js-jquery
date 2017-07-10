47203a60d40bf16248e5934443af9e7d7a2e4ed6
genarvaez <ge_naravez@hotmail.com>
2017-07-10 13:34:25 -0400

    renderizado de acticidades

--- js/script-index.js
+++ js/script-index.js
@@ -61,7 +61,12 @@ function renderActivities(activitiesArray) {
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	
+	var html = '<a href="#" class="item-activity"><span class="attribution">'+
+	'<span class="avatar"><img src="'+ recipe.userAvatar + '" class="image-avatar">'+
+	'</span><span class="meta"><span class="author">'+ recipe.userName + '</span> made '+
+	'<span class="recipe">'+ recipe.recipeName + '</span>: '+ recipe.text + '<span class="location">&mdash;'+ recipe.place + '</span></span></span>'+
+	'<div class="bg-image" style="background-image: url("'+ recipe.image + '");"></div></a>'
+	$(".list-activities").append(html);
 }
 
 
